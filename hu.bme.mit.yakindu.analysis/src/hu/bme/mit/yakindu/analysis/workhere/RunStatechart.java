c9fee78846a17fa9860c118bd72f20b6d74fa9d5
szianami <szianami@gmail.com>
2021-03-15 17:40:56 +0100

    3. feladat - Yakindu kódgenerátor

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
@@ -2,30 +2,49 @@ package hu.bme.mit.yakindu.analysis.workhere;
 
 import java.io.IOException;
 
-// import hu.bme.mit.yakindu.analysis.RuntimeService;
-// import hu.bme.mit.yakindu.analysis.TimerService;
+import hu.bme.mit.yakindu.analysis.RuntimeService;
+import hu.bme.mit.yakindu.analysis.TimerService;
 import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
-
+import java.util.Scanner;
 
 
 public class RunStatechart {
 	
 	public static void main(String[] args) throws IOException {
-//		ExampleStatemachine s = new ExampleStatemachine();
-//		s.setTimer(new TimerService());
-//		RuntimeService.getInstance().registerStatemachine(s, 200);
-//		s.init();
-//		s.enter();
-//		s.runCycle();
-//		print(s);
-//		s.raiseStart();
-//		s.runCycle();
-//		System.in.read();
-//		s.raiseWhite();
-//		s.runCycle();
-//		print(s);
-//		System.exit(0);
+		ExampleStatemachine s = new ExampleStatemachine();
+		Scanner scanner = new Scanner(System.in);
+		s.setTimer(new TimerService());
+		RuntimeService.getInstance().registerStatemachine(s, 200);
+		s.init();
+		s.enter();
+		s.runCycle();
+		print(s);
+		
+		boolean shouldExit = false;
+		while (!shouldExit) {
+			String input = scanner.nextLine();
+			switch (input) {
+				case "exit":
+					shouldExit = true;
+					break;
+				case "start":
+					s.raiseStart();
+					s.runCycle();
+					break;
+				case "white":
+					s.raiseWhite();
+					s.runCycle();
+					break;
+				case "black":
+					s.raiseBlack();
+					s.runCycle();
+					break;
+			
+			}
+			print(s);
+		}
+		System.exit(0);
 	}
 
 	public static void print(IExampleStatemachine s) {
